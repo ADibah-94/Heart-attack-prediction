6c3e62bc247593f1561cd02920b3c72335fdea13
Nur Adibah <103229211+ADibah-94@users.noreply.github.com>
2022-05-17 17:41:44 +0800

    Add files via upload

---
+++ Heart Attack prediction/heart_attack_deploy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 15:48:32 2022
+
+@author: HP
+"""
+
---
+++ Heart Attack prediction/heart_attack_prediction.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 09:32:04 2022
+
+@author: HP
+"""
+
+import pandas as pd
+
+#%% EDA
+
+# Step 1) Data loading
+
+df = pd.read_csv('heart.csv')
